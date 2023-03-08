e3d62cdc0e73645780c6d5c00816ad0546a5479a
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2023-03-08 04:45:57 +0300

    Initial commit

---
+++ src/zadanie5.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class zadanie5 {
+    //5. Пользователь вводит три дробных числа. Вывести те из них, квадратный корень которых меньше 2.
+    public static void main(String[] args) {
+
+        Scanner scannerX = new Scanner(System.in);
+        System.out.print("Please enter x: ");
+        double x = scannerX.nextDouble();
+
+        Scanner scannerY = new Scanner(System.in);
+        System.out.print("Please enter y: ");
+        double y = scannerY.nextDouble();
+
+        Scanner scannerZ = new Scanner(System.in);
+        System.out.print("Please enter z: ");
+        double z = scannerZ.nextDouble();
+
+
+    }
+}
