4e74edbf109ea3647668b4ddb69b540e952f1630
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2023-03-08 20:31:57 +0300

    Initial comment

--- src/zadanie2.java
+++ src/zadanie2.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class zadanie2 {
+import java.util.Scanner;
+
+public class zadanie2 {
+    //2. Пользователь вводит строку, выведите ее длину
+    public static void main(String[] args) {
+        //Источник инфо здесь: https://javarush.com/groups/posts/klass-scanner
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите строку: ");
+        String s = scanner.nextLine();
+        int len = s.length();
+        System.out.println(len);
+    }
 }
